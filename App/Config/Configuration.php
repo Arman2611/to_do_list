a878083434b93576b2f93fa412506f9798e5d699
unknown <arman2611m@gmail.com>
2022-08-02 18:33:44 +0400

    Testing...

--- App/Config/Configuration.php
+++ App/Config/Configuration.php
@@ -4,9 +4,15 @@ namespace App\Config;
 
 class Configuration
 {
-    const DB_HOST = '127.0.0.1';
-    const DB_DATABASE = 'to_do_list';
+//    const DB_HOST = '127.0.0.1';
+//    const DB_DATABASE = 'to_do_list';
+//    const DB_CHARSET = 'utf8';
+//    const DB_USERNAME = 'root';
+//    const DB_PASSWORD = '';
+
+    const DB_HOST = 'sql8.freemysqlhosting.net';
+    const DB_DATABASE = 'sql8510398';
     const DB_CHARSET = 'utf8';
-    const DB_USERNAME = 'root';
-    const DB_PASSWORD = '';
+    const DB_USERNAME = 'sql8510398';
+    const DB_PASSWORD = 'xM8QPWTLdR';
 }
--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-//spl_autoload_register();
+
 spl_autoload_register(function ($class) {
     $class = str_replace('\\', '/', $class);
-//    var_dump($class);
     require_once($class.'.php');
 });
 
