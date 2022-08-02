df11862eda6e02ca19b204a48b741d163ba894c5
unknown <arman2611m@gmail.com>
2022-08-02 18:02:27 +0400

    Testing...

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-spl_autoload_register();
+//spl_autoload_register();
 spl_autoload_register(function ($class) {
-    var_dump($class);
+    $class = str_replace('\\', '/', $class);
+//    var_dump($class);
     require_once($class.'.php');
 });
 
