81ed5e8b44d907d1f74971e5ab2824b34a76bc07
unknown <arman2611m@gmail.com>
2022-08-02 17:46:32 +0400

    Testing...

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<?php spl_autoload_register(function ($class) {
+<?php
+use App\Routes\Router;
+
+spl_autoload_register(function ($class) {
     require_once($class.'.php');
 });
-
-//require_once 'App/test.php';
-
-use App\Routes\Router;
 
 $url = strtok($_SERVER["REQUEST_URI"], '?');
 
